818a3e034a6a2706ccd09e94865093d2c08479da
moujtahid21 <abdelbasset.moujtahid@smail.th-koeln.de>
2022-01-31 12:27:00 +0100

    setting up

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,12 @@
 import React, {Component} from "react";
-import 
+import logo from 'public/logo.png';
+import './App.css';
+
+class App extends Component
+{
+    render()
+    {
+        return 
+    }
+}
+    export default App;
